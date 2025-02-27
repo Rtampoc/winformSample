cc4e5ba5a87f65a60b4ab6024e9b2698873fe1dd
Ralph <rap.tampoc@gmail.com>
2025-02-28 01:40:34 +0800

    Add project files.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,268 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFname = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtLname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rbMale = new System.Windows.Forms.RadioButton();
+            this.rbFemale = new System.Windows.Forms.RadioButton();
+            this.rbBoth = new System.Windows.Forms.RadioButton();
+            this.cmbxGender = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.cmbxBadge = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "BadgeNo";
+            // 
+            // txtFname
+            // 
+            this.txtFname.Location = new System.Drawing.Point(112, 77);
+            this.txtFname.Name = "txtFname";
+            this.txtFname.Size = new System.Drawing.Size(137, 22);
+            this.txtFname.TabIndex = 3;
+            this.txtFname.TextChanged += new System.EventHandler(this.txtFname_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Firstname";
+            // 
+            // txtLname
+            // 
+            this.txtLname.Location = new System.Drawing.Point(112, 105);
+            this.txtLname.Name = "txtLname";
+            this.txtLname.Size = new System.Drawing.Size(137, 22);
+            this.txtLname.TabIndex = 5;
+            this.txtLname.TextChanged += new System.EventHandler(this.txtLname_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(39, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Lastname";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(39, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Birthday";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(39, 164);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(56, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Gender";
+            // 
+            // rbMale
+            // 
+            this.rbMale.AutoSize = true;
+            this.rbMale.Location = new System.Drawing.Point(331, 77);
+            this.rbMale.Name = "rbMale";
+            this.rbMale.Size = new System.Drawing.Size(59, 21);
+            this.rbMale.TabIndex = 10;
+            this.rbMale.TabStop = true;
+            this.rbMale.Text = "Male";
+            this.rbMale.UseVisualStyleBackColor = true;
+            // 
+            // rbFemale
+            // 
+            this.rbFemale.AutoSize = true;
+            this.rbFemale.Location = new System.Drawing.Point(331, 108);
+            this.rbFemale.Name = "rbFemale";
+            this.rbFemale.Size = new System.Drawing.Size(75, 21);
+            this.rbFemale.TabIndex = 11;
+            this.rbFemale.TabStop = true;
+            this.rbFemale.Text = "Female";
+            this.rbFemale.UseVisualStyleBackColor = true;
+            // 
+            // rbBoth
+            // 
+            this.rbBoth.AutoSize = true;
+            this.rbBoth.Location = new System.Drawing.Point(331, 136);
+            this.rbBoth.Name = "rbBoth";
+            this.rbBoth.Size = new System.Drawing.Size(58, 21);
+            this.rbBoth.TabIndex = 12;
+            this.rbBoth.TabStop = true;
+            this.rbBoth.Text = "Both";
+            this.rbBoth.UseVisualStyleBackColor = true;
+            // 
+            // cmbxGender
+            // 
+            this.cmbxGender.FormattingEnabled = true;
+            this.cmbxGender.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cmbxGender.Location = new System.Drawing.Point(112, 161);
+            this.cmbxGender.Name = "cmbxGender";
+            this.cmbxGender.Size = new System.Drawing.Size(137, 24);
+            this.cmbxGender.TabIndex = 13;
+            this.cmbxGender.TextChanged += new System.EventHandler(this.cmbxGender_TextChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy-MM-dd";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(112, 133);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(137, 22);
+            this.dateTimePicker1.TabIndex = 14;
+            this.dateTimePicker1.Value = new System.DateTime(2025, 2, 7, 23, 56, 0, 0);
+            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(42, 228);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.TabIndex = 15;
+            this.btnEdit.Text = "Edit";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Location = new System.Drawing.Point(174, 228);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.TabIndex = 16;
+            this.btnAccept.Text = "Accept";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(255, 228);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 17;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(350, 169);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(75, 23);
+            this.btnExtract.TabIndex = 18;
+            this.btnExtract.Text = "Extract";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            this.btnExtract.Click += new System.EventHandler(this.btnExtract_Click);
+            // 
+            // cmbxBadge
+            // 
+            this.cmbxBadge.FormattingEnabled = true;
+            this.cmbxBadge.Location = new System.Drawing.Point(112, 24);
+            this.cmbxBadge.Name = "cmbxBadge";
+            this.cmbxBadge.Size = new System.Drawing.Size(137, 24);
+            this.cmbxBadge.TabIndex = 19;
+            this.cmbxBadge.SelectedIndexChanged += new System.EventHandler(this.cmbxBadge_SelectedIndexChanged);
+            this.cmbxBadge.SelectedValueChanged += new System.EventHandler(this.cmbxBadge_SelectedValueChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 269);
+            this.Controls.Add(this.cmbxBadge);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.btnEdit);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.cmbxGender);
+            this.Controls.Add(this.rbBoth);
+            this.Controls.Add(this.rbFemale);
+            this.Controls.Add(this.rbMale);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtLname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtFname);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFname;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtLname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RadioButton rbMale;
+        private System.Windows.Forms.RadioButton rbFemale;
+        private System.Windows.Forms.RadioButton rbBoth;
+        private System.Windows.Forms.ComboBox cmbxGender;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.ComboBox cmbxBadge;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Text.RegularExpressions;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "SERVER=(local)\\RAP;Database=dbData;USER=SA;PWD=1234";
+        private bool isUpdating = false;
+        private bool isNewRecord = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            enable();
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            string badge = cmbxBadge.Text;
+            string firstName = txtFname.Text;
+            string lastName = txtLname.Text;
+            DateTime birthday = dateTimePicker1.Value;
+            string gender = cmbxGender.SelectedItem.ToString();
+
+            if (!Regex.IsMatch(badge, @"^[SMP]\d{6}$"))
+            {
+                MessageBox.Show("BadgeNo must start with 'S', 'M', or 'P' followed by 6 digits.", "Invalid BadgeNo", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd;
+
+                if (isUpdating)
+                {
+                    cmd = new SqlCommand("UPDATE tblData SET FirstName=@FirstName, LastName=@LastName, Birthday=@Birthday, Gender=@Gender WHERE BadgNo=@BadgNo", conn);
+                }
+                else
+                {
+                    cmd = new SqlCommand("INSERT INTO tblData (BadgNo, FirstName, LastName, Birthday, Gender) VALUES (@BadgNo, @FirstName, @LastName, @Birthday, @Gender)", conn);
+                }
+
+                cmd.Parameters.AddWithValue("@BadgNo", badge);
+                cmd.Parameters.AddWithValue("@FirstName", firstName);
+                cmd.Parameters.AddWithValue("@LastName", lastName);
+                cmd.Parameters.AddWithValue("@Birthday", birthday);
+                cmd.Parameters.AddWithValue("@Gender", gender);
+
+
+                //SqlCommand cmd = new SqlCommand("INSERT INTO Persons (BadgNo, FirstName, LastName, Birthday, Gender) VALUES (@BadgNo, @FirstName, @LastName, @Birthday, @Gender)", conn);
+                //cmd.Parameters.AddWithValue("@BadgNo", badge);
+                //cmd.Parameters.AddWithValue("@FirstName", firstName);
+                //cmd.Parameters.AddWithValue("@LastName", lastName);
+                //cmd.Parameters.AddWithValue("@Birhtday", birthday);
+                //cmd.Parameters.AddWithValue("@Gender", gender);
+
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show(isUpdating ? "BadgeNo updated successfully!" : "BadgeNo added successfully!");
+                    LoadData();
+                    btnAccept.Enabled = false;
+                    //btnAccept.Text = "Add";
+                    isUpdating = false;
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            disable();
+            LoadData();
+            SetAcceptButtonState();
+        }
+
+        private void LoadData()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString)){
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT BadgNo FROM tblData", conn);
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    cmbxBadge.Items.Add(reader["BadgNo"].ToString());
+                }
+                reader.Close();
+            }
+        }
+
+        private void cmbxBadge_SelectedValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbxBadge_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string badge = cmbxBadge.SelectedItem.ToString();
+            LoadDetails(badge);
+            isNewRecord = false;
+            SetAcceptButtonState();
+        }
+
+        private void LoadDetails(string badge)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString)) {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Firstname, Lastname, Birthday, Gender FROM tblData WHERE BadgNo = @BadgNo", conn);
+                cmd.Parameters.AddWithValue("BadgNo", badge);
+
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    txtFname.Text = reader["Firstname"].ToString();
+                    txtLname.Text = reader["Lastname"].ToString();
+                    dateTimePicker1.Value = Convert.ToDateTime(reader["Birthday"]);
+                    cmbxGender.SelectedItem = reader["Gender"].ToString();
+                }
+                reader.Close();
+            }
+        }
+
+        private void disable()
+        {
+            txtFname.Enabled = false;
+            txtLname.Enabled = false;
+            dateTimePicker1.Enabled = false;
+            cmbxGender.Enabled = false;
+        }
+        private void enable()
+        {
+            txtFname.Enabled = true;
+            txtLname.Enabled = true;
+            dateTimePicker1.Enabled = true;
+            cmbxGender.Enabled = true;
+        }
+
+        private void SetAcceptButtonState()
+        {
+            btnAccept.Enabled = isNewRecord;
+        }
+
+        private void txtFname_TextChanged(object sender, EventArgs e)
+        {
+            isNewRecord = true;
+            SetAcceptButtonState();
+        }
+
+        private void txtLname_TextChanged(object sender, EventArgs e)
+        {
+            isNewRecord = true;
+            SetAcceptButtonState();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+            isNewRecord = true;
+            SetAcceptButtonState();
+        }
+
+        private void cmbxGender_TextChanged(object sender, EventArgs e)
+        {
+            isNewRecord = true;
+            SetAcceptButtonState();
+        }
+
+        private void ExportToExcel(DataTable dt)
+        {
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workBook = excelApp.Workbooks.Add();
+            Excel.Worksheet workSheet = (Excel.Worksheet)workBook.Sheets[1];
+
+            for (int i = 0; i < dt.Columns.Count; i++)
+            {
+                workSheet.Cells[1, i + 1] = dt.Columns[i].ColumnName;
+            }
+
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    workSheet.Cells[i + 2, j + 1] = dt.Rows[i][j];
+                }
+            }
+            excelApp.Visible = true;
+        }
+
+        private void btnExtract_Click(object sender, EventArgs e)
+        {
+            string genderFilter = "";
+            if (rbMale.Checked)
+            {
+                genderFilter = "Male";
+            }
+            else if (rbFemale.Checked)
+            {
+                genderFilter = "Female";
+            }
+
+            DataTable dataTable = new DataTable();
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd;
+                if (genderFilter == "")
+                {
+                    cmd = new SqlCommand("SELECT BadgNo, FirstName, LastName, Birthday, Gender FROM tblData", conn);
+                }
+                else
+                {
+                    cmd = new SqlCommand("SELECT BadgNo, FirstName, LastName, Birthday, Gender FROM tblData WHERE Gender = @Gender", conn);
+                    cmd.Parameters.AddWithValue("@Gender", genderFilter);
+                }
+
+                SqlDataReader reader = cmd.ExecuteReader();
+                dataTable.Load(reader);
+                reader.Close();
+            }
+
+            ExportToExcel(dataTable);
+        }
+    }
+}
